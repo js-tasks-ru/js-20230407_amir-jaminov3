0294495b48127c97a4ab87d6feb8e82e1c49a385
amir.jaminov <amir.jaminov@gmail.com>
2023-04-19 17:17:34 +0200

    Use localeCompare with locale attribute ['ru', 'en']

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,17 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  return function (obj) {
+    let prop = obj;
 
+    for (const field of path.split('.')) {
+      if (!Object.keys(prop).includes(field)) {
+        return undefined;
+      }
+
+      prop = prop[field];
+    }
+
+    return prop;
+  };
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,30 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === undefined) {
+    size = string.length;
+  }
 
+  if (size <= 0) {
+    return '';
+  }
+
+  let result = '';
+  let character = string[0];
+  let counter = 0;
+
+  for (const i in string) {
+    if (counter < size || character !== string[i]) {
+      if (character !== string[i]) {
+        counter = 0;
+        character = string[i];
+      }
+
+      result += string[i];
+    }
+
+    counter++;
+  }
+
+  return result;
 }
