100d857ed7b3a585e8b44098a2a8f7db099a4b60
amir.jaminov <amir.jaminov@gmail.com>
2023-05-01 22:57:32 +0200

    Event practice

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,75 @@
 class Tooltip {
-  initialize () {
+  static instance;
+  static flag;
 
+  constructor() {
+    if (!Tooltip.instance) {
+      Tooltip.instance = this;
+    }
+
+    return Tooltip.instance;
+  }
+
+  initialize () {
+    this.render();
+  }
+
+  render () {
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = `<div class="tooltip"></div>`;
+
+    this.element = wrapper.firstElementChild;
+
+    document.body.append(this.element);
+
+    this.addListeners();
+  }
+
+  addListeners () {
+    const divs = document.querySelectorAll('div');
+    const element = Tooltip.instance.element;
+
+    for (const div of divs) {
+      div.addEventListener('pointerover', function (e) {
+        if (e.defaultPrevented) {
+          return false;
+        }
+
+        if (e.target.dataset.tooltip) {
+          document.body.append(element);
+          Tooltip.flag = true;
+
+          element.innerHTML = e.target.dataset.tooltip;
+          element.style.left = e.clientX + 'px';
+          element.style.top = e.clientY + 'px';
+          e.preventDefault();
+        }
+      });
+
+      div.addEventListener('pointermove', function (e) {
+        if (Tooltip.flag) {
+          element.style.left = e.clientX + 'px';
+          element.style.top = e.clientY + 'px';
+        }
+      });
+
+      div.addEventListener('pointerout', function (e) {
+        if (e.defaultPrevented) {
+          return false;
+        }
+
+        Tooltip.flag = false;
+        element.remove();
+
+        e.preventDefault();
+      });
+    }
+  }
+
+  destroy () {
+    if (this.element) {
+      this.element.remove();
+    }
   }
 }
 
